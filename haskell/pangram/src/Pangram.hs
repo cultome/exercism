027b7f84d14fbda22b5c9dafabbf1e75ebba4458
Carlos Soria <csoria@cultome.io>
2018-12-09 22:55:28 -0600

    Pangram solved

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -1,4 +1,12 @@
 module Pangram (isPangram) where
+import Data.Char
 
 isPangram :: String -> Bool
-isPangram text = error "You need to implement this function."
+isPangram text = countLetters (map (\x -> toLower x) text) == 26
+
+countLetters :: [Char] -> Int
+countLetters (x:xs) = if x `elem` xs || not (x `elem` ['a'..'z']) then
+                        countLetters xs
+                      else
+                        1 + countLetters xs
+countLetters [] = 0
